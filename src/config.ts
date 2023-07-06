206219bf2662dbc812710bc9be3381e4f42e282e
Vadim <vmakeev.dev@gmail.com>
2023-07-06 21:42:30 +0400

    Delete tabs

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,10 @@
 export const CONSTANTS = {
-    domain: 'http://'+ window.location.host,
-    // domain: 'http://pc.r-morozov.ru/',
+    domain: 'http://'+ window.location.host + '/',
     streams: 'api/streams/',
     streamInfo: 'api/stream_info/',
     read: 'api/read/',
     send: 'api/send/',
     consumers: 'api/consumers/',
     deleteConsumers: 'api/delete_consumer/',
-    // ws: 'pc.r-morozov.ru',
     ws: window.location.host,
 };
