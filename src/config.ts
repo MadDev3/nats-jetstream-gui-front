a1c5e56a6b4adf0bd455fe38e3aed72350d41fe1
Vadim <vmakeev.dev@gmail.com>
2023-07-06 18:24:51 +0400

    Change port

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,12 @@
 export const CONSTANTS = {
-    domain: 'http://pc.r-morozov.ru/',
+    domain: 'http://localhost:80/',
+    // domain: 'http://pc.r-morozov.ru/',
     streams: 'api/streams/',
     streamInfo: 'api/stream_info/',
     read: 'api/read/',
     send: 'api/send/',
     consumers: 'api/consumers/',
     deleteConsumers: 'api/delete_consumer/',
-    ws: 'pc.r-morozov.ru',
+    // ws: 'pc.r-morozov.ru',
+    ws: 'localhost:80',
 };
